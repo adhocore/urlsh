8d0ff880ebcbb0a0158e63029ea594bd947aad01
Jitendra Adhikari <jiten.adhikary@gmail.com>
2020-10-02 10:32:42 +0700

    feat(controller): add frontend controller

---
+++ controller/frontend.go
@@ -0,0 +1,11 @@
+package controller
+
+import (
+    "net/http"
+
+    "github.com/adhocore/urlsh/response"
+)
+
+func Index(res http.ResponseWriter, req *http.Request) {
+    response.JSON(res, http.StatusOK, response.Body{"message": "it works"})
+}
