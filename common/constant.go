6c93eae286acc58c7180dd80c17fb888a2ef7b7b
Jitendra Adhikari <jitendra@central.tech>
2020-10-02 20:09:36 +0700

    feat(common): add short code length constant

--- common/constant.go
+++ common/constant.go
@@ -1,4 +1,10 @@
 package common
 
-// DateLayout is standard yyyy-mm-dd format
-const DateLayout = "2006-01-02 15:04:05"
+const (
+    // DateLayout is standard yyyy-mm-dd format
+    DateLayout = "2006-01-02 15:04:05"
+
+    // ShortCodeLength is length of short code to be generated (5-12 chars)
+    ShortCodeLength = 6
+)
+
