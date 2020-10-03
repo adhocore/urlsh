4735648bac5980634f8c0b7e21d4e4a72cd3e7a6
Jitendra Adhikari <jiten.adhikary@gmail.com>
2020-10-03 11:29:13 +0700

    refactor(controller.client): simplify with body.Merge

--- controller/client.go
+++ controller/client.go
@@ -11,17 +11,18 @@ import (
 func CreateShortUrl(res http.ResponseWriter, req *http.Request) {
     shortCode, err := url.CreateUrlShortCodeFromRequest(req)
     shortUrl := fmt.Sprintf("%s%s%s", "http://", req.Host, "/" + shortCode)
+    body := response.Body{"short_code": shortCode, "short_url": shortUrl}
 
     if err == nil {
-        response.JSON(res, http.StatusOK, response.Body{"short_code": shortCode, "short_url": shortUrl})
+        response.JSON(res, http.StatusOK, body)
         return
     }
 
-    status, body := http.StatusUnprocessableEntity, response.Body{"message": err.Error()}
+    status, errBody := http.StatusUnprocessableEntity, response.Body{"message": err.Error()}
     if shortCode != "" {
         status = http.StatusConflict
-        body["short_code"] = shortCode
+        errBody.Merge(body)
     }
 
-    response.JSON(res, status, body)
+    response.JSON(res, status, errBody)
 }
