9251437458e0fc81a0a72cd53657f8186a5093cf
Jitendra Adhikari <jitendra@central.tech>
2020-10-03 20:31:35 +0700

    test(controller): test delete short url endpoint

--- controller/admin_test.go
+++ controller/admin_test.go
@@ -5,6 +5,7 @@ import (
     "math/rand"
     "os"
     "testing"
+    "time"
 )
 
 func TestListUrl(t *testing.T) {
@@ -15,10 +16,13 @@ func TestListUrl(t *testing.T) {
     })
 
     t.Run("list endpoint - ok", func(t *testing.T) {
+        rand.Seed(time.Now().UTC().UnixNano())
+
         _     = os.Setenv("APP_ALLOW_DUPE_URL", "1")
         url  := fmt.Sprintf("http://localhost:1000/very/long/url-%v", rand.Intn(1000000))
         body := TestBody{"url": url, "expires_on": "2030-01-01 00:00:00", "keywords": []string{"local"}}
         resp := request("POST", "/api/urls", body, CreateShortUrl)
+
         resp.assertStatus(200, t)
 
         t.Run("by page", func(t *testing.T) {
@@ -29,6 +33,36 @@ func TestListUrl(t *testing.T) {
         t.Run("by keyword", func(t *testing.T) {
             resp := request("GET", "/api/admin/urls?keyword=local", TestBody{}, ListUrl)
             resp.assertStatus(200, t)
+        })
+    })
+}
+
+func TestDeleteShortUrl(t *testing.T) {
+    t.Run("delete endpoint", func(t *testing.T) {
+        rand.Seed(time.Now().UTC().UnixNano())
+
+        body := TestBody{"url": fmt.Sprintf("https://localhost/test/delete/short/url/%v", rand.Intn(1000000))}
+        resp := request("POST", "/api/urls", body, CreateShortUrl)
+        resp.assertStatus(200, t)
+        resp.assertContains("short_code", t)
+
+        shortCode := resp["short_code"]
+
+        t.Run("delete - nok", func(t *testing.T) {
+            uri := fmt.Sprintf("/api/admin/urls?short_code=%v", rand.Intn(1000000))
+            resp := request("DELETE", uri, TestBody{}, DeleteShortUrl)
+            resp.assertStatus(404, t)
+        })
+
+        t.Run("delete - ok", func(t *testing.T) {
+            uri := fmt.Sprintf("/api/admin/urls?short_code=%v", shortCode)
+            resp := request("DELETE", uri, TestBody{}, DeleteShortUrl)
+            resp.assertStatus(200, t)
+
+            t.Run("delete - ok - nok", func(t *testing.T) {
+                resp = request("DELETE", uri, TestBody{}, DeleteShortUrl)
+                resp.assertStatus(404, t)
+            })
         })
     })
 }
