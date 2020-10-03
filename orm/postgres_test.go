b238859c71420de72e483e9b92bc9fe7d87cafe6
Jitendra Adhikari <jiten.adhikary@gmail.com>
2020-10-03 11:35:11 +0700

    test(orm): add tests for postgres connection

---
+++ orm/postgres_test.go
@@ -0,0 +1,11 @@
+package orm
+
+import "testing"
+
+func TestConnection(t *testing.T) {
+    t.Run("connection - OK", func(t *testing.T) {
+        conn := Connection()
+
+        conn.NowFunc()
+    })
+}
